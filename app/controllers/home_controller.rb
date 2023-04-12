b5cc8907e88b5209e8af985a57629c321f0eff94
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-04-12 15:11:45 +0300

    style updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,15 @@ class HomeController < ApplicationController
   end
 
   def result
-    @generated_text = generate_response(params[:text_prompt])
-    @generated_image = generate_image(params[:image_prompt])
+    generate_text = params[:text] == 'true'
+    generate_image = params[:image] == 'true'
+    
+    if generate_text
+      @generated_text = generate_response(params[:text_prompt])
+    end
+    
+    if generate_image
+      @generated_image = generate_image(params[:image_prompt])
+    end
   end
 end
